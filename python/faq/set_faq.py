34c186357c5cee841818d4f829a81ec1fd8c7ae9
BorisFN <boris.huang@futurenest.ai>
2024-10-09 15:47:11 +0800

    set faq

--- python/faq/set_faq.py
+++ python/faq/set_faq.py
@@ -1,32 +1,38 @@
 import requests
 
 # example BASE_URL: 'https://api.maiagent.ai/api/v1/'
-BASE_URL = "<base-api-url>"
-API_KEY = "<your-api-key>"
+BASE_URL = '<base-api-url>'
+API_KEY = '<your-api-key>'
 
-assert BASE_URL != "<base-api-url>", "Please set your base API URL"
-assert API_KEY != "<your-api-key>", "Please set your API key"
+assert BASE_URL != '<base-api-url>', 'Please set your base API URL'
+assert API_KEY != '<your-api-key>', 'Please set your API key'
 
 
-def set_faq(question, answer, chatbot_id):
-    url = f"{BASE_URL}api/faqs/"
+def set_faq(question: str, answer: str, chatbot_id: str) -> int:
+    url = f'{BASE_URL}api/faqs/'
 
-    data = {"question": question, "answer": answer, "chatbot": chatbot_id}
+    try:
+        response = requests.post(
+            url,
+            headers={'Authorization': f'Api-Key {API_KEY}'},
+            json={'question': question, 'answer': answer, 'chatbot': chatbot_id},
+        )
+    except requests.exceptions.RequestException as e:
+        print(response.text)  # type: ignore
+        print(e)
+        exit(1)
+    except Exception as e:
+        print(e)
+        exit(1)
 
-    response = requests.post(
-        url,
-        headers={"Authorization": f"Api-Key {API_KEY}"},
-        json=data,
-    )
-
-    return response.json()
+    return response.status_code
 
 
 # Example usage
-if __name__ == "__main__":
-    question = "這是什麼問題"
-    answer = "我不知道"
-    chatbot_id = "6f2fc12a-ca5e-4539-834d-e5d21181d199"
+if __name__ == '__main__':
+    question = 'question'
+    answer = 'answer'
+    chatbot_id = 'chatbot_id'
 
     result = set_faq(question, answer, chatbot_id)
     print(result)
