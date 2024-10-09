4916d0313008f7870f59994fe5cfadd9b44e9b69
BorisFN <boris.huang@futurenest.ai>
2024-10-09 20:39:20 +0800

    add faq fix

--- python/faq/add_faq.py
+++ python/faq/add_faq.py
@@ -1,30 +1,27 @@
-import os
 import requests
-from dotenv import load_dotenv
 
-from pathlib import Path
+BASE_URL = 'https://api.maiagent.ai/api/v1/'
+API_KEY = '<your-api-key>'
 
+CHATBOT_ID = '<your-chatbot-id>'
+QUESTION = '<your-question>'
+ANSWER = '<your-answer>'
 
-ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+assert API_KEY != '<your-api-key>', 'Please set your API key'
 
-DOT_ENV_PATH = ROOT_DIR / ".env"
-load_dotenv(DOT_ENV_PATH)
-
-BASE_URL = os.environ.get('BASE_URL', default='https://api.maiagent.ai/api/v1/')
-API_KEY = os.environ.get('API_KEY')
-
-
-assert API_KEY is not None, 'Please set your API key'
+assert QUESTION != '<your-question>', 'Please set your question'
+assert ANSWER != '<your-answer>', 'Please set your answer'
+assert CHATBOT_ID != '<your-chatbot-id>', 'Please set your chatbot id'
 
 
-def add_faq(question: str, answer: str, chatbot_id: str) -> int:
+def add_faq(chatbot_id: str, question: str, answer: str) -> int:
     url = f'{BASE_URL}faqs/'
 
     try:
         response = requests.post(
             url,
             headers={'Authorization': f'Api-Key {API_KEY}'},
-            json={'question': question, 'answer': answer, 'chatbot': chatbot_id},
+            json={'chatbot': chatbot_id,'question': question, 'answer': answer,},
         )
     except requests.exceptions.RequestException as e:
         print(response.text)  # type: ignore
@@ -39,9 +36,5 @@ def add_faq(question: str, answer: str, chatbot_id: str) -> int:
 
 # Example usage
 if __name__ == '__main__':
-    question = 'question'
-    answer = 'answer'
-    chatbot_id = 'chatbot_id'
-
-    result = add_faq(question, answer, chatbot_id)
+    result = add_faq(QUESTION, ANSWER, CHATBOT_ID)
     print(result)
