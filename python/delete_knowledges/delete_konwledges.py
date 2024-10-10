d4f478b2097af17ddbf68019984dc2cef3a506cf
BorisFN <boris.huang@futurenest.ai>
2024-10-10 21:06:21 +0800

    fix comment

--- python/delete_knowledges/delete_konwledges.py
+++ python/delete_knowledges/delete_konwledges.py
@@ -5,10 +5,11 @@ API_KEY = '<your-api-key>'
 
 CHATBOT_ID = '<your-chatbot-id>'
 
-FILES_ID = '<your-files-id>'
+FILE_ID = '<your-files-id>'
 
 assert API_KEY != '<your-api-key>', 'Please set your API key'
 assert CHATBOT_ID != '<your-chatbot-id>', 'Please set your chatbot id'
+assert FILE_ID != '<your-files-id>', 'Please set your files id'
 
 def delete_knowledge(files_id):
     url = f'{BASE_URL}chatbots/{CHATBOT_ID}/files/{files_id}/'
@@ -27,4 +28,4 @@ def delete_knowledge(files_id):
 
 
 if __name__ == "__main__":
-    delete_knowledge(FILES_ID)
+    delete_knowledge(FILE_ID)
